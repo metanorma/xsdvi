e776b6f7c8a43eb5ef6d92b4b6fddaf281efefc1
Intelligent2013 <dyuzhev@gmail.com>
2021-02-20 11:24:06 +0300

    command line parser added

--- src/main/java/xsdvi/XsdVi.java
+++ src/main/java/xsdvi/XsdVi.java
@@ -1,9 +1,18 @@
 package xsdvi;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
 import org.apache.xerces.xs.XSImplementation;
 import org.apache.xerces.xs.XSLoader;
@@ -29,34 +38,83 @@ public final class XsdVi {
 	private static List<String> inputs = new ArrayList<String>();
 	private static String style = null;
 	private static String styleUrl = null;
+       
+	/**
+	 * 
+	 */
+	public static final String EMBODY_STYLE = "embodyStyle";
+	
+	/**
+	 * 
+	 */
+	public static final String GENERATE_STYLE = "generateStyle";
+	
+	/**
+	 * 
+	 */
+	public static final String USE_STYLE = "useStyle";
+	
+        static final Option optionEmbodyStyle = Option.builder(EMBODY_STYLE)
+                    .desc(" css style will be embodied in each svg file, this is default")
+                    .required(true)
+                    .build();
+        
+        static final Option optionGenerateStyle = Option.builder(GENERATE_STYLE)
+                    .desc(" new css file with specified name will be generated and used by svgs")
+                    .hasArg()
+                    .required(true)
+                    .build();
+       
+        static final Option optionUseStyle = Option.builder(USE_STYLE)
+                    .desc(" external css file at specified url will be used by svgs")
+                    .hasArg()
+                    .required(true)
+                    .build();
+         
+        static final Options options = new Options() {
+            { 
+            }
+        };
 
+        static final Options optionsEmbodyStyle = new Options() {
+            {
+                addOption(optionEmbodyStyle);
+            }
+        };
+        
+        static final Options optionsGenerateStyle = new Options() {
+            {
+                addOption(optionGenerateStyle);
+            }
+        };
+        
+        static final Options optionsUseStyle = new Options() {
+            {
+                addOption(optionUseStyle);
+            }
+        };
+        
+        static final String CMD = "java -jar xsdvi.jar <input1.xsd> [<input2.xsd> [<input3.xsd> ...]]";
+        static final String CMD_EmbodyStyle = "java -jar xsdvi.jar <input1.xsd> [<input2.xsd> [<input3.xsd> ...]] -" + EMBODY_STYLE;
+        static final String CMD_GenerateStyle = "java -jar xsdvi.jar <input1.xsd> [<input2.xsd> [<input3.xsd> ...]] -" + GENERATE_STYLE;
+        static final String CMD_UseStyle = "java -jar xsdvi.jar <input1.xsd> [<input2.xsd> [<input3.xsd> ...]] -" + USE_STYLE;
+        
+        static final String USAGE = getUsage();
+        
+        static final int ERROR_EXIT_CODE = -1;
+        
 	/**
 	 * 
 	 */
-	public static final String EMBODY_STYLE = "-embodyStyle";
-	
-	/**
-	 * 
-	 */
-	public static final String GENERATE_STYLE = "-generateStyle";
-	
-	/**
-	 * 
-	 */
-	public static final String USE_STYLE = "-useStyle";
-	
-	/**
-	 * 
-	 */
-	public static final String USAGE =
+	/*public static final String USAGE =
 		"\n" +
 		"USAGE:\n" +
 		"java -jar xsdvi.jar <input1.xsd> [<input2.xsd> [<input3.xsd> ...]] [style]\n" +
 		"  STYLE:\n" +
-		"    " + EMBODY_STYLE + "                css style will be embodied in each svg file, this is default\n" +
-		"    " + GENERATE_STYLE + " <style.css>  new css file with specified name will be generated and used by svgs\n" +
-		"    " + USE_STYLE + "      <style.css>  external css file at specified url will be used by svgs\n";
-	
+		"    -" + EMBODY_STYLE + "                css style will be embodied in each svg file, this is default\n" +
+		"    -" + GENERATE_STYLE + " <style.css>  new css file with specified name will be generated and used by svgs\n" +
+		"    -" + USE_STYLE + "      <style.css>  external css file at specified url will be used by svgs\n";
+	*/
 	
 	/**
 	 * 
@@ -80,6 +138,7 @@ public final class XsdVi {
 		WriterHelper writerHelper = new WriterHelper();
 		SvgForXsd svg = new SvgForXsd(writerHelper);
 		
+                
 		if (style.equals(EMBODY_STYLE)) {
 			logger.info("The style will be embodied");
 			svg.setEmbodyStyle(true);
@@ -129,7 +188,71 @@ public final class XsdVi {
 	 * @param args
 	 */
 	private static void parseArgs(String[] args) {
-		if (args.length < 1 || args[0].equalsIgnoreCase(EMBODY_STYLE) || args[0].equalsIgnoreCase(GENERATE_STYLE) || args[0].equalsIgnoreCase(USE_STYLE)) {
+            
+            CommandLineParser parser = new DefaultParser();
+               
+            boolean cmdFail = false;
+
+            try {
+                CommandLine cmd = parser.parse(options, args);
+                style = EMBODY_STYLE;
+                
+                inputs.addAll(cmd.getArgList());
+                
+                return;
+            } catch (ParseException exp) {
+                cmdFail = true;
+            }
+
+            if(cmdFail) {
+                try {
+                    CommandLine cmd = parser.parse(optionsEmbodyStyle, args);
+                    style = EMBODY_STYLE;
+                    
+                    inputs.addAll(cmd.getArgList());
+                    
+                    return;
+                } catch (ParseException exp) {
+                    cmdFail = true;
+                }
+            }
+
+            if(cmdFail) {
+                try {
+                    CommandLine cmd = parser.parse(optionsGenerateStyle, args);
+                    style = GENERATE_STYLE;
+                    styleUrl = cmd.getOptionValue(GENERATE_STYLE);
+                    
+                    inputs.addAll(cmd.getArgList());
+                    
+                    return;
+                } catch (ParseException exp) {
+                    cmdFail = true;
+                }
+            }
+
+            if(cmdFail) {
+                try {
+                    CommandLine cmd = parser.parse(optionsUseStyle, args);
+                    style = USE_STYLE;
+                    styleUrl = cmd.getOptionValue(USE_STYLE);
+                    
+                    inputs.addAll(cmd.getArgList());
+                    
+                    return;
+                } catch (ParseException exp) {
+                    cmdFail = true;
+                }
+            }
+
+            if (cmdFail) {
+                printUsage();
+                System.exit(ERROR_EXIT_CODE);
+            }
+            
+            
+            
+		/*if (args.length < 1 || args[0].equalsIgnoreCase(EMBODY_STYLE) || args[0].equalsIgnoreCase(GENERATE_STYLE) || args[0].equalsIgnoreCase(USE_STYLE)) {
 			printUsage();
 			System.exit(1);
 		}
@@ -163,8 +286,8 @@ public final class XsdVi {
 			else {
 				inputs.add(args[i]);
 			}
-		}
-		style = EMBODY_STYLE;
+		}*/
+		
 	}
 
 	/**
@@ -199,4 +322,20 @@ public final class XsdVi {
 		}
 		return schemaLoader;
 	}
+        
+    private static String getUsage() {
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter pw = new PrintWriter(stringWriter);
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp(pw, 100, CMD, "", options, 0, 0, "");
+        pw.write("\nOR\n\n");
+        formatter.printHelp(pw, 100, CMD_EmbodyStyle, "", optionsEmbodyStyle, 0, 0, "");
+        pw.write("\nOR\n\n");
+        formatter.printHelp(pw, 100, CMD_GenerateStyle, "", optionsGenerateStyle, 0, 0, "");
+        pw.write("\nOR\n\n");
+        formatter.printHelp(pw, 100, CMD_UseStyle, "", optionsUseStyle, 0, 0, "");
+        pw.flush();
+        return stringWriter.toString();
+    }
+
 }
